9b46eb82ebc263a1242f5728decd16d8ee8863b9
xu <xf.xu@wowtech-inc.com>
2025-11-14 15:50:13 +0800

    tc

---
+++ an_lian_app/lib/i18n/tr.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class AnLianTrs extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+    'en': {'app name': 'AN'},
+    'zh': {'app name': '安连'},
+  };
+}
